20cc6ee0c9d17b444ec69d862e3f5763a8641c72
Benjamin Cohen <bac1087@gmail.com>
2019-01-02 23:48:09 -0500

    Updates

---
+++ precache-manifest.f0fbca90574e66629b2e4db23dff59d3.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "01c4e8db95f44233ae6964c02d99e4da",
+    "url": "/whack-a-mole/static/media/mole.01c4e8db.png"
+  },
+  {
+    "revision": "016a06f68bf9eb9302551f960bc513e4",
+    "url": "/whack-a-mole/static/media/mallet.016a06f6.png"
+  },
+  {
+    "revision": "1b92b0047b2d6e306d752af8b2bcc3ee",
+    "url": "/whack-a-mole/static/media/grass-texture.1b92b004.png"
+  },
+  {
+    "revision": "4a5d32f7fa9753454757",
+    "url": "/whack-a-mole/static/js/runtime~main.4a5d32f7.js"
+  },
+  {
+    "revision": "8212ad46a37919c0fcc8",
+    "url": "/whack-a-mole/static/js/main.8212ad46.chunk.js"
+  },
+  {
+    "revision": "ad554e6ac194b347d2a0",
+    "url": "/whack-a-mole/static/js/1.ad554e6a.chunk.js"
+  },
+  {
+    "revision": "8212ad46a37919c0fcc8",
+    "url": "/whack-a-mole/static/css/main.e13bce35.chunk.css"
+  },
+  {
+    "revision": "8af664f57e28a6795d1133e551bf8f5b",
+    "url": "/whack-a-mole/index.html"
+  }
+];
